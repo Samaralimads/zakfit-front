2d55b9f0429444c0c603bacab3e7649ec672b03f
“Samara <samara.limads@gmail.com>
2025-11-25 12:24:10 +0100

    first commit

---
+++ zakfit-front/zakfit_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  zakfit_frontApp.swift
+//  zakfit-front
+//
+//  Created by Samara Lima da Silva on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct zakfit_frontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
